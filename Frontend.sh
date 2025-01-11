df4fbe7e0fb8ed62b744ec7c1a7414271512193f
saidangeti <saidangeti1443@gmail.com>
2025-01-11 15:38:17 +0530

    chanfed config file

--- Frontend.sh
+++ Frontend.sh
@@ -5,5 +5,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-cp E:\RAGHU PROJECTS\Expense-shell\test-28-1-23\expense.conf /etc/nginx/default.d/expense.conf 
+cp expense.conf /etc/nginx/default.d/expense.conf 
 systemctl restart nginx 
